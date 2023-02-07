fe51df6e0331e143a9bd836d008ad6dfc54651cc
Kateryna Tonapetian <106622532+Diverona@users.noreply.github.com>
2023-02-07 04:24:19 +0200

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+
+  const getObject = (data) =>
+    data.reduce((acc, obj) => {
+      acc[obj[0]] = obj[1];
+      return acc;
+    }, {});
+
+  let obj = getObject(bracketsConfig);
+  if (str.length % 2 !== 0) {
+    return false;
+  } else {
+    for (let i = 0; i < str.length; i++) {
+      let curr = str[i];
+      let lastStackElem = stack[stack.length - 1];
+
+      if (curr === obj[lastStackElem]) {
+        stack.pop();
+      } else if (curr in obj) {
+        stack.push(curr);
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
